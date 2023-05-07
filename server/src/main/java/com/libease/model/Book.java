6f7e510e03366fb5770956f1e5d3a659b1c33b64
murata <pu59244@gmail.com>
2023-05-08 04:30:18 +0900

    無駄な空白の修正

--- server/src/main/java/com/libease/model/Book.java
+++ server/src/main/java/com/libease/model/Book.java
@@ -4,16 +4,17 @@ public class Book {
     private int bookId;
     private String bookName;
     private String author;
-    
+
     public Book(int bookId, String bookName, String author) {
         this.bookId = bookId;
         this.bookName = bookName;
         this.author = author;
     }
-    
+
     public Book () {
 
     }
+
     public int getBookId() {
         return bookId;
     }
