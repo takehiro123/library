4e8de6fb3e0a4899e67319387e5d85460758834f
murata <pu59244@gmail.com>
2023-05-08 12:03:25 +0900

    イラン参照削除

--- server/src/main/java/com/libease/common/PathManager.java
+++ server/src/main/java/com/libease/common/PathManager.java
@@ -1,8 +1,9 @@
 package com.libease.common;
 
- public  class  PathManager {
+public class PathManager {
     public final static String USER_LIST_SERACH = "/WEB-INF/user_list_search.jsp";
-    private PathManager () {
+
+    private PathManager() {
 
     }
 }
