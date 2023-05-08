fb8b5c25d80d687605dc1f3d78104dd03d4b74c8
murata <pu59244@gmail.com>
2023-05-08 11:58:23 +0900

    メッセージ管理クラス作成

---
+++ server/src/main/java/com/libease/common/PathManager.java
@@ -0,0 +1,8 @@
+package com.libease.common;
+
+ public  class  PathManager {
+    public final static String USER_LIST_SERACH = "/WEB-INF/user_list_search.jsp";
+    private PathManager () {
+
+    }
+}
