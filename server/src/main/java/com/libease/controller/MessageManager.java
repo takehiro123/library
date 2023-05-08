ef72ebc4aaa4a7f5321ddf4dd44809441128a8b0
murata <pu59244@gmail.com>
2023-05-08 12:00:01 +0900

    メッセージ管理クラスの作成

---
+++ server/src/main/java/com/libease/controller/MessageManager.java
@@ -0,0 +1,9 @@
+package com.libease.controller;
+
+public class MessageManager {
+    public final static String SAMPLE_MSG = "こうやって定義するといいかもよ";
+
+    private MessageManager (){
+        
+    }
+}
