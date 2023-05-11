ef4c8fd056d461936155b8126631e0a0eae40657
murata <pu59244@gmail.com>
2023-05-11 17:02:14 +0900

    ログアウト処理の作成

--- server/src/main/java/com/libease/controller/UserController.java
+++ server/src/main/java/com/libease/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.libease.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 public class UserController {
     public void Login() {
 
     } 
-    public void Logout() {
-        
+    /**
+     * @param request
+     * ログアウト処理を行う　この処理を実行するとセッションに格納されている情報をすべて削除するため注意
+     */
+    public void Logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
     }
 }
